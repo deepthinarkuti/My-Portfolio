881f9779a8b01d92d097fb9901214236b64dc84d
deepthi narkuti <deepthinarkuti@yahoo.com>
2020-08-16 14:40:39 -0400

    Added projects and profile pic alignments

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -5,7 +5,7 @@ import { Link } from 'react-scroll';
 import { Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 import quickshare from './images/quick-share.jpg';
 import gesturerecognition from './images/gesture-recognition.jpg';
-import carparking from './images/car-parking.png';
+import carparking from './images/carpparking.png';
 import download from './images/download.jpg';
 import jpmorgan from './images/jpmorgan1.png';
 
@@ -33,7 +33,7 @@ class Landing extends Component {
                                     color: '#fff', height: '250px', backgroundImage: `url(${quickshare})`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat'
                                 }} ></CardTitle>
                                 <CardText><b>
-                                    QUICK-SHARE App allows sharing multiple files, images asynchronously across multiple devices in which the user is loggedin</b>
+                                    QUICK-SHARE App enables user to copy the text, files, images on a device and shares across multiple devices of the user asynchronously</b>
                                 </CardText>
                                 <CardActions border>
                                     <p><b>C#, Cosmos DB, Microsoft Azure Cloud</b></p>
@@ -50,7 +50,7 @@ class Landing extends Component {
                                 </CardText>
                                 <CardActions border>
                                     <p><b>Open CV, Python</b></p>
-                                    <b><a href="https://github.com/deepthinarkuti/Virtual-Keyboard-Handling-using-Hand-Gesture-Recognition">Git hub</a></b>
+                                    <b><a href="https://github.com/deepthinarkuti/Gesture-Recognition">Git hub</a></b>
                                 </CardActions>
                             </Card>
                             {/* Project 3 */}
@@ -59,25 +59,25 @@ class Landing extends Component {
                                     color: '#fff', height: '260px', backgroundImage: `url(${carparking})`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat'
                                 }} ></CardTitle>
                                 <CardText><b>
-                                    EASY-PARKING is a dashboard to select the slots for parking the car with in multiple levels</b>
+                                    EASY-PARKING helps in searching and booking a slot, to park a car quickly & easily in a parking lot with multiple levels</b>
                                 </CardText>
                                 <CardActions border>
                                     <p><b>Java, Bootstrap, Cloud</b></p>
-                                    <b><a href="https://github.com/deepthinarkuti/Easy-parking">Git hub</a></b>
+                                    <b><a href="https://github.com/deepthinarkuti/Easy-Parking">Git hub</a></b>
                                 </CardActions>
                             </Card>
 
                             {/* Project 4 */}
                             <Card shadow={5} style={{ width: '23%', margin: 'auto' }}>
                                 <CardTitle style={{
-                                    color: '#fff', height: '260px', backgroundImage: `url(${download})`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat'
+                                    color: '#fff', height: '250px', backgroundImage: `url(${download})`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat'
                                 }} ></CardTitle>
                                 <CardText><b>
-                                    SMART-BUG-DETECTOR suggests top 5 solutions to identify and resolve the defects quickly and efficiently</b>
+                                    SMART-BUG-DETECTOR suggests top 5 solutions to the defects to resolve the defects quickly and efficiently based on the content and words match logic</b>
                                 </CardText>
                                 <CardActions border>
                                     <p><b>Java, SQL</b></p>
-                                    <b><a href="https://github.com/deepthinarkuti/Smart-Bug-Detector">Git hub</a></b>
+                                    <b><a href="https://github.com/deepthinarkuti/Smart-Defect-Advisor">Git hub</a></b>
                                 </CardActions>
                             </Card>
                         </div>
@@ -108,7 +108,7 @@ class Landing extends Component {
                         </div>
                         <hr />
                         <h4><b>Connect with me</b></h4>
-                        <div className="social-links" style={{ width: '22%', margin: 'auto' }}>
+                        <div className="social-links" style={{ margin: 'auto' }}>
 
                             {/* Email */}
                             <a href="mailto:deepthinarkuti@ufl.edu" rel="noopener noreferrer" target="_blank">
--- src/components/projects.js
+++ src/components/projects.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 import quickshare from './images/quick-share.jpg';
 import gesturerecognition from './images/gesture-recognition.jpg';
-import carparking from './images/car-parking.png';
+import carparking from './images/carpparking.png';
 import download from './images/download.jpg';
 
 class Projects extends Component {
@@ -15,7 +15,7 @@ class Projects extends Component {
                         color: '#fff', height: '200px', backgroundImage: `url(${quickshare})`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat'
                     }} ></CardTitle>
                     <CardText><b>
-                        QUICK-SHARE App allows sharing multiple files, images asynchronously across multiple devices in which the user is loggedin</b>
+                        QUICK-SHARE App enables user to copy the text, files, images on a device and shares across multiple devices of the user asynchronously</b>
                     </CardText>
                     <CardActions border>
                         <p><b>C#, Cosmos DB, Microsoft Azure Cloud</b></p>
@@ -32,7 +32,7 @@ class Projects extends Component {
                     </CardText>
                     <CardActions border>
                         <p><b>Open CV, Python</b></p>
-                        <b><a href="https://github.com/deepthinarkuti/Virtual-Keyboard-Handling-using-Hand-Gesture-Recognitione">Git hub</a></b>
+                        <b><a href="https://github.com/deepthinarkuti/Gesture-Recognition">Git hub</a></b>
                     </CardActions>
                 </Card>
 
@@ -42,11 +42,11 @@ class Projects extends Component {
                         color: '#fff', height: '200px', backgroundImage: `url(${carparking})`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat'
                     }} ></CardTitle>
                     <CardText><b>
-                        EASY-PARKING is a dashboard to select the slots for parking the car with in multiple levels</b>
+                        EASY-PARKING facilitates in searching and booking a slot, to park a car quickl & easily in a parking lot with multiple levels</b>
                     </CardText>
                     <CardActions border>
                         <p><b>Java, Bootstrap, Cloud</b></p>
-                        <b><a href="https://github.com/deepthinarkuti/Easy-parking">Git hub</a></b>
+                        <b><a href="https://github.com/deepthinarkuti/Easy-Parking">Git hub</a></b>
                     </CardActions>
                 </Card>
 
@@ -56,11 +56,11 @@ class Projects extends Component {
                         color: '#fff', height: '200px', backgroundImage: `url(${download})`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat'
                     }} ></CardTitle>
                     <CardText><b>
-                        SMART-BUG-DETECTOR suggests top 5 solutions to identify and resolve the defects quickly and efficiently</b>
+                        SMART-BUG-DETECTOR suggests top 5 solutions to the defects to resolve the defects quickly and efficiently based on the content and words match logic</b>
                     </CardText>
                     <CardActions border>
                         <p><b>Java, SQL</b></p>
-                        <b><a href="https://github.com/deepthinarkuti/Smart-Bug-Detector">Git hub</a></b>
+                        <b><a href="https://github.com/deepthinarkuti/Smart-Defect-Advisor">Git hub</a></b>
                     </CardActions>
                 </Card>
             </div>
